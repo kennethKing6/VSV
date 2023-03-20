ae549f9f62e917e49d0fa66e4b178542ae640f46
Kenneth Kouadio <kennethemmanuelziguy28@gmail.com>
2023-03-20 00:35:43 +0000

    feat: vvs footer

--- src/components/Footer/VVSFooter.js
+++ src/components/Footer/VVSFooter.js
@@ -1,30 +1,34 @@
-import { Grid } from '@mui/material'
+import { Link, Grid } from '@mui/material'
 import React from 'react'
 import { Colors } from '../../theme/colors'
 import MediumPaddingLayout from '../layout/mediumPaddingLayout'
-import MediumPaddingTopBottom from '../layout/mediumPaddingTopBottom'
-import MediumSpacingLayout from '../layout/mediumSpacingLayout'
 import NoSpacingLayout from '../layout/NoSpacingLayout'
+import SmallSpacingLayout from '../layout/smallSpacingLayout'
 import MediumTitle from '../Text/MediumTitle'
 import SmallTitle from '../Text/SmallTitle'
 
 export default function VVSFooter() {
+
+
+
     return (
         <MediumPaddingLayout bgColor={Colors.primary_black}>
             <Grid container >
                 <Grid item xs={12} md={4}>
                     <MediumTitle text={"Téléphone"} />
-                    <SmallTitle text={"(+225) 27 22 00 72 03"} />
-                    <SmallTitle text={"(+225) (07 58 54 69 80"} />
-                    <SmallTitle text={"(+225) 05 55 80 41 84"} />
-
+                    <SmallSpacingLayout> <Link href="tel:27 22 00 72 03">(+225) 27 22 00 72 03</Link></SmallSpacingLayout>
+                    <SmallSpacingLayout><Link variant="contained" href="tel:07 58 54 69 80">(+225) 07 58 54 69 80</Link></SmallSpacingLayout>
+                    <SmallSpacingLayout><Link variant="contained" href="tel:05 55 80 41 84">(+225) 05 55 80 41 84</Link></SmallSpacingLayout>
                 </Grid>
                 <Grid item xs={12} md={4}>
                     <MediumTitle text={"Courriel"} />
-                    <SmallTitle text={"ongvivresansviolence@gmail.com"} />
+                    <Link href="mailto:ongvivresansviolence@gmail.com">ongvivresansviolence@gmail.com</Link>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <MediumTitle text={"Siège"} />
+                    <SmallTitle text={"Bingerville - Palmeraie"} />
 
                 </Grid>
-                <Grid item xs={12} md={4}></Grid>
             </Grid>
 
         </MediumPaddingLayout>
