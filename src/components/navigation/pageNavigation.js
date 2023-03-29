8eac6c4400418366f38328a95f294ea7403b6b24
Kenneth Kouadio <kennethemmanuelziguy28@gmail.com>
2023-03-29 11:30:57 +0000

    fix: improve look and feel of header

--- src/components/navigation/pageNavigation.js
+++ src/components/navigation/pageNavigation.js
@@ -1,41 +1,79 @@
 import { Grid, Avatar, Link } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { ASSETS } from '../../assets/assets'
 import { APP18n } from '../../i18n/i18n'
 import { Colors } from '../../theme/colors'
-import {  Navbar, Nav } from 'react-bootstrap';
 import { APPFONT } from '../../fonts/font'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+let  selection = null; 
 export default function PageNavigation() {
 
     const navigation = useNavigate()
 
     return (
         <Grid container  >
-            <Navbar expand="md">
-                <Navbar.Brand onClick={()=>navigation("/")}><Avatar sx={{ bgcolor: Colors.primary_yellow, width: 100, height: 50 }} variant="square" src={ASSETS.logo_svg} /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" style={{position:'relative',left:'70%'}}/>
+            <Navbar expand="md" style={{ width: '100%' }}>
+                <Navbar.Brand style={{cursor:'pointer'}} onClick={() => {
+                    navigation("/")
+                    selection = null
+                }}><Avatar sx={{ bgcolor: Colors.primary_yellow, width: 100, height: 50, }} variant="square" src={ASSETS.logo_svg} /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className="mr-auto" >
-                        <Nav.Link onClick={()=>navigation("/library")}    style={styles.links}>{APP18n.translate(APP18n.getKeys().navigation_libraries)}</Nav.Link>
-                        <Nav.Link  onClick={()=>navigation("/video")} style={styles.links}>{APP18n.translate(APP18n.getKeys().navigation_video)}</Nav.Link>
-                        <Nav.Link onClick={()=>navigation("/audio")} style={styles.links}>{APP18n.translate(APP18n.getKeys().navigation_audio)}</Nav.Link>
-                        <Nav.Link  onClick={()=>navigation("/activity")} style={styles.links}>{APP18n.translate(APP18n.getKeys().navigation_activities)}</Nav.Link>
-
+                        <Nav.Link
+                            onClick={() => {
+                                selection = 'library'
+                                navigation("/library")
+                            }}
+                            style={styles(selection,'library')}>
+                            {APP18n.translate(APP18n.getKeys().navigation_libraries)}
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                selection = 'video'
+                                navigation("/video")
+                            }}
+                            style={styles(selection,'video')}>
+                            {APP18n.translate(APP18n.getKeys().navigation_video)}
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                selection = 'audio'
+                                navigation("/audio")
+                            }}
+                            style={styles(selection,'audio')}>
+                            {APP18n.translate(APP18n.getKeys().navigation_audio)}
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                selection = 'activity'
+                                navigation("/activity")
+                            }}
+                            style={styles(selection,'activity')}>
+                            {APP18n.translate(APP18n.getKeys().navigation_activities)}
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-
         </Grid>
     )
+
 }
 
-const styles = {
-    links: {
+const styles = (selection,type)=>{
+    
+    if(selection === type) return  {
         color: Colors.primary_white,
-        fontFamily:APPFONT.getFontKeys().fontNameSlabo
-    },
-    linkLayout: {
-
+        fontFamily: APPFONT.getFontKeys().fontNameSlabo,
+        opacity: 1
     }
+    else return {
+        opacity: 0.6,
+        color: Colors.primary_white,
+        fontFamily: APPFONT.getFontKeys().fontNameSlabo,
+    }
+    
 }
