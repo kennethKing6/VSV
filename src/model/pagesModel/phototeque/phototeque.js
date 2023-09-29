eee790b05c422d3c30c443298758f574b897abee
Kenneth Kouadio <kennethemmanuelziguy28@gmail.com>
2023-09-29 16:01:16 -0700

    fix: phototeque works with the gcp api

--- src/model/pagesModel/phototeque/phototeque.js
+++ src/model/pagesModel/phototeque/phototeque.js
@@ -1,5 +1,6 @@
 import { API_URL, } from "../../api/Baseurl";
 import { APIQuery } from "../../api/apiQuery";
+import { formatImageUrl } from "../../utils/ExtractFirebaseExtURL";
 
 
 class PhototequeList {
@@ -11,24 +12,31 @@ class PhototequeList {
             this.rowList = []
         }
     }
-    #setup(list = []) {
-        this.rowList = list.map((value) => {
+     #setup(list = []) {
+        this.rowList =  list.map((value) => {
 
-            const { attributes } = value
+            const { categorie, photo,published_at:publishedAt } = value
 
-            const { titre, photo,publishedAt } = attributes
-            const { data = [] } = photo
+            
+         let imagesList = []
 
-            let imagesList = []
-
-            data.forEach(element => {
-                const { attributes: itemAttributes } = element
-                const { url } = itemAttributes
-                const finalUrl = url.includes("https") ? url : API_URL + url
-                imagesList.push(finalUrl)
+            //Extract the firebase url 
+          if(Array.isArray(photo)){
+            photo.forEach(element => {
+                const { url } = element
+               imagesList.push(formatImageUrl(url))
 
             });
+          }else{
+            const { url } = photo
+            
+            imagesList.push(formatImageUrl(url))
 
+          }
+            //Get the real url of the photo through firebase
+            console.log("imagesList",imagesList)
+
+            // Display the date into the format
             let myDate = new Date(publishedAt);
             let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
             let frenchDate = myDate.toLocaleDateString('fr-FR', options);
@@ -36,7 +44,7 @@ class PhototequeList {
 
 
             return {
-                title: titre,
+                title: categorie,
                 photoList: imagesList,
                 publishedDate:frenchDate
             }
@@ -49,23 +57,23 @@ class PhototequeList {
     }
 }
 
+
 export class Phototeque extends APIQuery {
 
     async getPhotos(page = 1) {
         try {
             const response = await this.paginateWithPageNum(`${API_URL}/photoques`, page)
-            const { data = [], meta } = response
+
 
             //Extract data
 
-            const photoList = new PhototequeList(data).getLibraryList()
+            const photoList = new PhototequeList(response).getLibraryList()
+
 
             // Extract pageSize
-            const { pagination } = meta
-            const { pageCount } = pagination
             return {
                 carousel: photoList.reverse(),
-                pageSize: pageCount,
+                pageSize: response.length,
             }
 
 
@@ -75,4 +83,5 @@ export class Phototeque extends APIQuery {
     }
 
 }
+
 
---
+++ src/model/utils/ExtractFirebaseExtURL.js
@@ -0,0 +1,21 @@
+import { API_URL } from "../api/Baseurl"
+
+/**
+ * 
+ * @param {*} url format is 
+ * 
+ * "https://storage.googleapis.com/gs://vivre-sans-violence-1cb0b.appspot.com/thumbnail_3qYDYcgWr6zFdF-Image_created_with_a_mobile_phone.jpg"
+ * 
+ * and we want to remove gs://
+ */
+export const extractFirebaseExtUrl = (url = '')=>{
+    const splitArray = url.split("gs://")
+    return splitArray[0] + splitArray[1]
+}
+
+export const formatImageUrl = (url = '')=>{
+    const finalUrl = url.includes("https") ? url : API_URL + url
+    const cleanedUrl = extractFirebaseExtUrl(finalUrl)
+    console.log("^^^^^^^^",cleanedUrl)
+    return cleanedUrl;
+}
