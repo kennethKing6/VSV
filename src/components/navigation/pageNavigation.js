492971c0e689fa3a6fbd76f00ca674d69dabd52e
Kenneth Kouadio <kennethemmanuelziguy28@gmail.com>
2023-09-30 14:46:10 -0700

    refactor: Hide pages that do not yet have content to be displayed

--- src/components/navigation/pageNavigation.js
+++ src/components/navigation/pageNavigation.js
@@ -1,17 +1,20 @@
 import { Grid, Avatar, } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { ASSETS } from '../../assets/assets'
-import { APP18n } from '../../i18n/i18n'
+// import { APP18n } from '../../i18n/i18n'
 import { Colors } from '../../theme/colors'
-import { APPFONT } from '../../fonts/font'
+// import { APPFONT } from '../../fonts/font'
 import { useLocation, useNavigate, } from 'react-router-dom'
-import Nav from 'react-bootstrap/Nav';
+// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
 export default function PageNavigation() {
 
-    const [actualPage, setActualPage] = useState('/')
+    const [
+        // actualPage
+        , 
+        setActualPage] = useState('/')
     const navigation = useNavigate()
     const location = useLocation();
 
@@ -41,7 +44,7 @@ export default function PageNavigation() {
                 <Navbar.Brand style={{ cursor: 'pointer' }} onClick={() => {
                     navigation("/")
                 }}><Avatar sx={{ bgcolor: Colors.primary_yellow, width: 100, height: 50, }} variant="square" src={ASSETS.logo_svg} /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className="mr-auto" >
                         <Nav.Link
@@ -73,23 +76,23 @@ export default function PageNavigation() {
                             {APP18n.translate(APP18n.getKeys().navigation_activities)}
                         </Nav.Link>
                     </Nav>
-                </Navbar.Collapse>
+                </Navbar.Collapse> */}
             </Navbar>
         </Grid>
     )
 
 }
 
-const styles = (selection, type) => {
-    if (selection === type) return {
-        color: Colors.primary_white,
-        fontFamily: APPFONT.getFontKeys().fontNameMontserrat,
-        opacity: 1
-    }
-    else return {
-        opacity: 0.6,
-        color: Colors.primary_white,
-        fontFamily: APPFONT.getFontKeys().fontNameMontserrat,
-    }
+// const styles = (selection, type) => {
+//     if (selection === type) return {
+//         color: Colors.primary_white,
+//         fontFamily: APPFONT.getFontKeys().fontNameMontserrat,
+//         opacity: 1
+//     }
+//     else return {
+//         opacity: 0.6,
+//         color: Colors.primary_white,
+//         fontFamily: APPFONT.getFontKeys().fontNameMontserrat,
+//     }
 
-}
+// }
