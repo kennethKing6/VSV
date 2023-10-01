a75b43e0ddc4268a794b4a9aa00396361201e5ef
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2023-09-30 20:06:40 -0700

    refactor:  remove unused aws sdk

--- src/model/api/Baseurl.js
+++ src/model/api/Baseurl.js
@@ -1,3 +1,4 @@
 export const BASE_URL = "https://api.ongvivresansviolence.org"
 export const API_URL = "https://strapi-server-s2xarhclna-uc.a.run.app"
 export const DEV_URL = "http://localhost:1337"
+export const SECONDARY_API_URL = "https://good-ruby-giraffe-tam.cyclic.app"
--- src/model/aws/awsEmail.js
+++ src/model/aws/awsEmail.js
@@ -1,23 +1,10 @@
-import AWS from 'aws-sdk';
-import { API_URL } from '../api/Baseurl';
-
-
-
-AWS.config.update({
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID_ENV,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY_ENV,
-    region: process.env.REACT_APP_AWS_REGION_ENV
-});
+import { SECONDARY_API_URL } from "../api/Baseurl"
 
 export class AWSEmail {
-    constructor() {
-        this.ses = new AWS.SES({ apiVersion: '2010-12-01' });
-
-    }
 
     async senEmail(name, surname, message, email) {
 
-        const url = API_URL + '/contact-us'
+        const url = `${SECONDARY_API_URL}/contact-us`
 
         return await fetch(url, {
             method: 'POST',
